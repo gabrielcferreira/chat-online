53c5f1222ae4c99892c804dde4244538b9968fc8
gabrielcferreira <88871314+gabrielcferreira@users.noreply.github.com>
2024-11-30 19:33:08 -0300

    Inclusão html e estruturação

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import Room, Message
 
 def home(request):
     rooms = Room.objects.all()
-    messages = Message.objects.all()
-    return render(request, 'index.html', {
-        'rooms': rooms,
-        'messages': messages
+    
+    return render(request, 'chat/home.html', {
+        'rooms': rooms
     })
+
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'chat/list-messages.html' 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # o super() permite herdar o método da classe pai e sobrescrever
+        return context
 
 
   
